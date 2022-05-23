14750724e923af6d55c7371db39d5dc8df7ce869
wolfgang <wwang@plexxis.com>
2022-05-23 23:40:34 +0800

    test for orders

---
+++ src/models/tests/Order_spec.ts
@@ -0,0 +1,91 @@
+import { Order, OrderStore } from '../Order'
+import { User, UserStore }  from '../User'
+
+const store = new OrderStore(); 
+
+describe('Product Model', ()=>{
+    it('should have an index method', ()=>{
+        expect(store.index).toBeDefined()
+    })
+
+    it('should have an show method', ()=>{
+        expect(store.show).toBeDefined()
+    })
+
+    it('should have an create method', ()=>{
+        expect(store.create).toBeDefined()
+    })
+
+    it('should have an delete method', ()=>{
+        expect(store.delete).toBeDefined()
+    })
+
+    const user: User = {
+        id: 1, 
+        firstname: 'testFirstName',
+        lastname: 'testLastName',
+        password: 'testPassword'
+    }
+
+    const order: Order = {
+        id: 1,
+        user_id: 1,
+        status: 1
+    }
+
+    beforeAll(async function(done) {
+        await new UserStore().create(user)
+        done()
+    })
+
+    afterAll(async function(done) {
+        await new UserStore().delete(user.id.toString())
+        done()
+    })
+
+    describe('adding an element at the beginning and removing at the end', () => {
+        beforeAll(async function(done) {
+            await store.create(order)
+            done()
+        })
+
+        afterAll(async function(done) {
+            await store.delete('1')
+            done()
+        })
+
+        it('index method should return a list of items', async () => {
+            const rows = await store.index()
+            expect(rows).toEqual([order])
+        })
+    
+        it('show method should should list by id', async () => {
+            const row = await store.show('1')
+            expect(row).toEqual(order)
+        })
+    })
+
+    describe('removing the element at the end', () => {
+        afterAll(async function(done){
+            await store.delete('1')
+            done()
+        })
+
+        it('create method should add an item', async () => {
+            const row: Order = await store.create(order)
+            expect(row).toEqual(order)
+        })
+    })
+
+    describe('adding an element at the beginning', () => {
+        beforeAll(async function(done) {
+            await store.create(order)
+            done()
+        })
+        
+        it('delete method should be able to delete item', async () => {
+            const rows = await store.delete('1')
+            expect(rows).toEqual(order)
+        })
+    })
+})
